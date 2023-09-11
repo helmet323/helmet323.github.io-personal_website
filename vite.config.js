d557bbbf2c39b5b4528e3344e0639e233365efc8
Tommy Su <su.tommy23@gmail.com>
2023-09-11 11:54:10 -0400

    commmit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personal_website',
+  base: '/helmet323.github.io-personal_website',
   plugins: [react()],
 });
